cdafd730f3d86d836ff621cc573ff551fda27b37
Javier Valero <javiervaleroenriquez@outlook.com>
2022-02-15 10:14:22 +0100

    added i18n

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var i18n = require("i18n-express");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +17,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(i18n({
+    translationsPath: path.join(__dirname, 'i18n'), // <--- use here. Specify translations files path.
+    siteLangs: ["es", "pl", "en"],
+    textsVarName: 'translation'
+}));
 
 module.exports = app;
